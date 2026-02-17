cbdc531601ee97fcdc5c6fd76773538c81934f06
Sajal069 <sharmasajal069@gmail.com>
2026-02-17 19:11:54 +0530

    Add production server requirements for backend

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,11 @@
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
 from app.extensions import socketio
 
 app = create_app()
 
 if __name__ == "__main__":
+    # This is for local development
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
